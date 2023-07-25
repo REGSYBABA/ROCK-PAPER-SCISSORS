2b86186b6145c0d7c799d272a31988df497e5f53
Omoregha Michael <Regsybaba@Reghas-MacBook-Air.local>
2023-07-25 16:14:28 +0100

    added a new javascript file with a better fuunctionality

---
+++ app2.js
@@ -0,0 +1,41 @@
+let compchoice = ["rock", "paper", "scissors"]
+const playerSelection = "rock";
+// let computerSelection = getComputersChoice();
+let playerScore = 0
+let computerScore = 0
+
+function getComputersChoice(){
+    const randomIndex = Math.floor(Math.random() * compchoice.length)
+    const item = compchoice[randomIndex]
+    console.log(item)
+    return item
+
+}
+// getComputersChoice()
+function game(){
+    for (let i = 0; i < 5; i++){
+        const computerSelection = getComputersChoice();
+        console.log(playRound(playerSelection, computerSelection))
+    }
+}
+game()
+
+function playRound(playerSelection, computerSelection){
+    // console.log(1, playerSelection, 2, computerSelection)
+    if (playerSelection == 'scissors' && computerSelection == 'paper' 
+             || playerSelection == 'paper' && computerSelection == 'rock' 
+             || playerSelection == 'rock' && computerSelection == 'scissors'){
+                playerScore++
+                return "Player Wins"
+        }else if (computerSelection == 'scissors' && playerSelection == 'paper' 
+            || computerSelection == 'paper' && playerSelection == 'rock' 
+            || computerSelection == 'rock' && playerSelection == 'scissors'){
+                computerScore++
+                console.log(computerScore)
+            return "Computer Wins"
+        }else {
+            return "it's a tie"
+        } 
+}
+
+
